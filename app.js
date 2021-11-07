5a71dc67e49e12ee223258e05d791d66fdd480af
josip555 <josip.arelic00@gmail.com>
2021-11-07 23:21:03 +0100

    added source code

---
+++ app.js
@@ -0,0 +1,104 @@
+const https = require('https');
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+
+const app = express();
+require('dotenv').config();
+const {
+  auth,
+  requiresAuth
+} = require('express-openid-connect');
+const {
+  response
+} = require('express');
+const { equal, notEqual } = require('assert');
+const port = process.env.PORT || 3000;
+
+if(process.env.PORT){
+  app.listen(port, () => {
+    console.log(`Listening at port ${port}`)
+  });
+} else {
+  let server = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+  }, app);
+  server.listen(port, () => console.log(`Listening at port ${port}...`));
+}
+
+app.use(express.static(__dirname + '/public/'));
+app.use(express.json({
+  limit: '1mb'
+}));
+
+let map = new Map();
+let array = [];
+
+app.use(
+  auth({
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL || `https://localhost:${port}`,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET,
+    idpLogout: true
+  })
+);
+
+app.post('/locate', (req, res) => {
+  console.log(req.body);
+  let nick = '';
+  if (req.oidc.isAuthenticated()) nick = req.oidc.user.nickname;
+  const lat = req.body.lat;
+  const lng = req.body.lng;
+  const ts = req.body.ts;
+  let userLocs = [];
+
+  if (req.oidc.isAuthenticated()) {
+    let b = false;
+    for(const e of array){
+      if (nick.valueOf() == e.valueOf()) b = true;
+    }
+    if (b) {
+      array.splice(array.indexOf(nick), 1);
+    }
+    array.push(nick);
+    map[nick] = { lat, lng, ts };
+
+    var l = 5;
+    if (array.length < 5) l = array.length;
+    let copy = [...array];
+    for(let i = 0; i < l; i++){
+      let user_nick = copy.pop();
+      let user_lat = map[user_nick].lat;
+      let user_lng = map[user_nick].lng;
+      let user_ts = map[user_nick].ts;
+      userLocs.push({ user_nick, user_lat, user_lng, user_ts });
+    }
+  }
+
+  res.json({
+    status: 'succes',
+    isAuthenticated: req.oidc.isAuthenticated(),
+    nickname: req.oidc.isAuthenticated() ? req.oidc.user.nickname : null,
+    userLocations: req.oidc.isAuthenticated() ? userLocs : null,
+    latitude: req.body.lat,
+    longitude: req.body.lng,
+    timestamp: req.body.ts
+  });
+
+  res.end();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+  res.render;
+  res.end();
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+  res.end();
+});
---
+++ public/scripts.js
@@ -0,0 +1,68 @@
+let mapOptions = {
+    center: [45.80101, 15.97157],
+    zoom: 10
+}
+
+let map = new L.map('map', mapOptions);
+
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+let marker = new L.Marker([45.80101, 15.97157]);
+
+if ('geolocation' in navigator) {
+
+    navigator.geolocation.getCurrentPosition(async position => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        const ts = position.timestamp;
+
+        const data = {
+            lat,
+            lng,
+            ts
+        };
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+        const res = await fetch('/locate', options);
+        const json = await res.json();
+        console.log(json);
+        const response = JSON.parse(JSON.stringify(json));
+
+        let latLng = [lat, lng];
+        document.getElementById('sidebar_title').textContent = `Na karti je označena vaša lokacija`;
+        document.getElementById('sidebar_comment').textContent = `Vaše koordinate: ${latLng}`;
+
+        if (response.userLocations == null) {
+            marker.setLatLng(latLng);
+            marker.addTo(map);
+            map.panTo(latLng);
+        } else {
+            document.getElementById('sidebar_title').textContent = `Dobrodošli ${response.nickname}!`;
+            document.getElementById('sidebar_comment').textContent = `Vaše koordinate: ${latLng}`;
+            document.getElementById('sidebar_comment2').textContent = `Na karti su također označene lokacije do 5 zadnjih korisnika(uključujući vas) i vrijeme kad su bili prijavljeni. Stisnite na pokazivač kako bi vidjeli detalje korisnika(username, koordinate, datum i vrijeme prijave).`
+
+            let mArray = [];
+            for (const e of response.userLocations) {
+                let uMarker = new L.Marker([e.user_lat, e.user_lng]);
+                var date = new Date(e.user_ts);
+                uMarker.addTo(map);
+                uMarker.bindPopup(`${`<p>${e.user_nick}</br>` + e.user_lat},${e.user_lng}</br>` + date.toLocaleDateString('hr-HR') + " " + date.toLocaleTimeString('hr-HR'));
+                mArray.push(uMarker);
+            }
+            var group = new L.featureGroup(mArray);
+            map.fitBounds(group.getBounds().pad(0.2));
+        }
+
+    });
+
+} else {
+
+    console.log('geolocation not available');
+
+}
